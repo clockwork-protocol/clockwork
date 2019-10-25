b0dece80a5fa2bf9aa8faf0e052443db82115056
Stephen Young <stephen101@gmail.com>
2019-10-25 18:59:43 +0200

    Created stubs for RecurringPaymentWallet

---
+++ test/testRecurringPaymentWallet.js
@@ -0,0 +1,17 @@
+contract("RecurringPaymentWallet", accounts => {
+    //should be able to fund the wallet
+    //should be able to withdraw from wallet
+    //should only allow the owner of a wallet to withdraw funds
+    //should not be able to withdraw more than you deposit
+    //should be able to create a recurring payment
+    //should only allow wallet owner to create a recurring payment
+    //should be able to fund a transaction for a recurring payment
+    //should only allow recurring payments created by this wallet to fund transactions
+    //should generate due transactions
+    //should have a list of due transactions
+
+    //not required for PoC
+    //should be able to cancel a recurring payment
+    //should be able to pause a recurring payment
+    //should be able to list all your recurring payments
+});
