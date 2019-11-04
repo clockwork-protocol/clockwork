44a9819cd5d1f1498e37796f3df3e339b9f2163c
Stephen Young <stephen101@gmail.com>
2019-11-04 13:06:41 +0200

    More config for travis

--- truffle-config.js
+++ truffle-config.js
@@ -4,6 +4,14 @@ module.exports = {
           host: "127.0.0.1",
           port: 7545,
           network_id: "*"
-      }
+      },
+      test: {
+        host: 'localhost',
+        port: 9545,
+        network_id: '*',
+        gas: 6.5e6,
+        gasPrice: 5e9,
+        websockets: true
+      },
   }
 };
